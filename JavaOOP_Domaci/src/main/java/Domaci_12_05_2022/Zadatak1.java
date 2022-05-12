b6931f94593e131a7d238b9426ee3ddab56b8e3d
Jovana Gojkovic <potic.jovana@gmail.com>
2022-05-12 23:26:06 +0200

    Minor change to Zadatak1

---
+++ JavaOOP_Domaci/src/main/java/Domaci_12_05_2022/Zadatak1.java
@@ -0,0 +1,8 @@
+package Domaci_12_05_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("Zdravo!");
+        System.out.println("DDDd");
+    }
+}
